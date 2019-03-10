6564f591eb8b2c8f8534ed0d93fc80e51669a706
Kirill Mikhel <kirill.mikhel@gmail.com>
2019-03-10 17:58:01 +0100

    Add player one movement and animations

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 50;
+
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        
+        Stop();
+    }
+
+    public void Up()
+    {
+        _rigidbody2D.velocity = speed * Vector2.up;
+        AlignToGrid("X");
+        TriggerAnimation("Up");
+    }
+
+    public void Down()
+    {
+        _rigidbody2D.velocity = speed * Vector2.down;
+        AlignToGrid("X");
+        TriggerAnimation("Down");
+    }
+
+
+    public void Right()
+    {
+        _rigidbody2D.velocity = speed * Vector2.right;
+        AlignToGrid("Y");
+        TriggerAnimation("Right");
+    }
+
+    public void Left()
+    {
+        _rigidbody2D.velocity = speed * Vector2.left;
+        AlignToGrid("Y");
+        TriggerAnimation("Left");
+    }
+
+    public void Stop()
+    {
+        _rigidbody2D.velocity = Vector2.zero;
+        _animator.speed = 0;
+    }
+
+    private void TriggerAnimation(string direction)
+    {
+        _animator.speed = 1f;
+        _animator.SetTrigger(direction);
+    }
+
+    private void AlignToGrid(string axis)
+    {
+        var position = new Vector2(_rigidbody2D.position.x, _rigidbody2D.position.y);
+
+        if (axis == "X")
+        {
+            position.x = Mathf.Round(position.x / 16) * 16;
+        }
+        else
+        {
+            position.y = Mathf.Round(position.y / 16) * 16;
+        }
+
+        _rigidbody2D.position = position;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Move _move;
+
+    private bool _isMovingUp = false;
+    private bool _isMovingDown = false;
+    private bool _isMovingRight = false;
+    private bool _isMovingLeft = false;
+
+    private void Awake()
+    {
+        _move = GetComponent<Move>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            if (!_isMovingUp)
+            {
+                _move.Up();
+                _isMovingUp = true;
+            }
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            if (!_isMovingDown)
+            {
+                _move.Down();
+                _isMovingDown = true;
+            }
+        }
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            if (!_isMovingRight)
+            {
+                _move.Right();
+                _isMovingRight = true;
+            }
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            if (!_isMovingLeft)
+            {
+                _move.Left();
+                _isMovingLeft = true;
+            }
+        }
+
+        if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0)
+        {
+            if (_isMovingUp || _isMovingDown || _isMovingRight || _isMovingLeft)
+            {
+                _move.Stop();
+                _isMovingUp = false;
+                _isMovingDown = false;
+                _isMovingRight = false;
+                _isMovingLeft = false;
+            }
+        }
+    }
+}
